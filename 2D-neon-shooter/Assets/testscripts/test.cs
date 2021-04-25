a9a77eb401c92da8005cc84e08f6e9defe3418de
Tom <tom.martens01@gmail.com>
2021-04-25 14:59:03 +0200

    First movement test

--- 2D-neon-shooter/Assets/testscripts/test.cs
+++ 2D-neon-shooter/Assets/testscripts/test.cs
@@ -4,37 +4,43 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-
-    public string printText;
     public float speed;
     public bool killPlayer = false;
+
+    public GameObject ball;
 
     // Start is called before the first frame update
     void Start()
     {
-        print("Started - text: " + printText);
-
-        gameObject.GetComponent<Renderer>().material.color = Color.green;
+        //gameObject.GetComponent<Renderer>().material.color = Color.green;
+        //InvokeRepeating("spawnBall", 0.5f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(speed, 0, 0);
+        //transform.Translate(speed, 0, 0);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Invoke("spawnBall", 0f);
+            transform.Translate(speed, 0, 0);
+        }
 
         if (killPlayer)
         {
             Destroy(gameObject);
         }
+
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        transform.Translate(xInput * speed, yInput * speed, 0);
+
     }
 
-    void OnEnable()
+    void spawnBall()
     {
-        print("Enabled");
-    }
-
-    void OnDisable()
-    {
-        print("Disabled");
+        Instantiate(ball, transform.position + new Vector3(0, -0.5f, 0), Quaternion.identity);
     }
 }
